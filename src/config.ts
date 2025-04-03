da8bbb7c963879787d40ce656543276a3a709481
Keduoli03 <2801429414@qq.com>
2025-04-04 03:20:21 +0800

    博客配置

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Lanke", //网站标题
-	subtitle: "Demo Site", //网站副标题
+	subtitle: "兴致所至", //网站副标题
 	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -54,7 +54,7 @@ export const navBarConfig: NavBarConfig = {
 
 // 个人资料配置
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Lorem Ipsum",
 	bio: "请为一切不真实之物骄傲，因为我们高于这个世界！",
 	links: [
