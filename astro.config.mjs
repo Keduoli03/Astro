1823e0dc2b078ac6205d99a49ec240f291346b74
Keduoli03 <2801429414@qq.com>
2025-04-08 11:21:24 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astro.blueke.top/",
+	site: "https://www.blueke.top/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
