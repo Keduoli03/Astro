90625bf671a213b20181c7cd4ce5be63c840c918
Keduoli03 <2801429414@qq.com>
2025-05-15 18:22:58 +0800

    111

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,7 +9,16 @@ const postsCollection = defineCollection({
 			draft: z.boolean().optional().default(false),
 			description: z.string().optional().default(""),
 			image: z.string().optional().default(""),
-			tags: z.array(z.string()).optional().default([]),
+			// tags: z.array(z.string()).optional().default([]),
+			tags: z
+				.union([
+					z
+						.string()
+						.transform((val) => [val]), // 单字符串转数组
+					z.array(z.string()), // 直接支持数组
+				])
+				.optional()
+				.default([]),
 			category: z.string().optional().default(""),
 			lang: z.string().optional().default(""),
 			series: z.string().optional(),
