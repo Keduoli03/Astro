f45903d4ccf65cb020630306ebffa77ff6128bc3
Keduoli03 <2801429414@qq.com>
2025-05-18 20:19:49 +0800

    111

---
+++ src/data/chronoboxData.js
@@ -0,0 +1,102 @@
+export const timelineItems = [
+	{
+		type: "anime",
+		title: "时光流逝，饭菜依旧美味",
+		cover:
+			"https://gcore.jsdelivr.net/gh/Keduoli03/My_img@img/%E6%97%B6%E5%85%89%E6%B5%81%E9%80%9D%EF%BC%8C%E9%A5%AD%E8%8F%9C%E4%BE%9D%E6%97%A7%E7%BE%8E%E5%91%B3.webp",
+		date: "2025-04-13",
+		url: "https://baike.baidu.com/item/%E5%B2%81%E6%9C%88%E6%B5%81%E9%80%9D%E9%A5%AD%E8%8F%9C%E4%BE%9D%E6%97%A7%E7%BE%8E%E5%91%B3/65543356",
+		episodes: "连载中",
+		rating: 4.9,
+		tags: ["喜剧", "剧情", "成长"],
+	},
+	{
+		type: "anime",
+		title: "Re:从零开始的异世界生活",
+		cover:
+			"https://gcore.jsdelivr.net/gh/Keduoli03/My_img@img/img/从零开始的异世界生活webp.webp",
+		date: "2016-04-04",
+		url: "https://mzh.moegirl.org.cn/Re:%E4%BB%8E%E9%9B%B6%E5%BC%80%E5%A7%8B%E7%9A%84%E5%BC%82%E4%B8%96%E7%95%8C%E7%94%9F%E6%B4%BB",
+		episodes: "更新至第三季下半",
+		rating: 4.9,
+		tags: ["奇幻", "冒险", "剧情", "成长"],
+	},
+	{
+		type: "anime",
+		title: "进击的巨人",
+		cover:
+			"https://gcore.jsdelivr.net/gh/Keduoli03/My_img@img/img/进击的巨人.jpg",
+		date: "2023-09-15",
+		url: "https://mzh.moegirl.org.cn/%E8%BF%9B%E5%87%BB%E7%9A%84%E5%B7%A8%E4%BA%BA",
+		episodes: 25,
+		rating: 4.8,
+		tags: ["热血", "奇幻"],
+	},
+	{
+		type: "anime",
+		title: "龙与虎",
+		cover: "https://gcore.jsdelivr.net/gh/Keduoli03/My_img@img/img/龙与虎.webp",
+		date: "2008-10-01",
+		url: "https://mzh.moegirl.org.cn/%E9%BE%99%E4%B8%8E%E8%99%8E",
+		episodes: 25,
+		rating: 4.8,
+		tags: ["爱情", "校园"],
+	},
+	{
+		type: "anime",
+		title: "间谍过家家",
+		cover:
+			"https://gcore.jsdelivr.net/gh/Keduoli03/My_img@img/img/间谍过家家.webp",
+		date: "2022-10-01",
+		desc: "震撼结局，自由的定义引发深思",
+		url: "https://example.com/aot",
+		episodes: 25,
+		rating: 4.8,
+		tags: ["爱情", "喜剧"],
+	},
+	{
+		type: "music",
+		title: "爱在西元前",
+		artist: "周杰伦",
+		cover:
+			"https://gcore.jsdelivr.net/gh/Keduoli03/My_img@img/%E4%BE%9D%E7%84%B6%E8%8C%83%E7%89%B9%E8%A5%BF.jpg",
+		album: "BOOTLEG",
+		date: "2023-08-02",
+		platform: {
+			qqmusic:
+				"https://i.y.qq.com/v8/playsong.html?songid=97740#webchat_redirect",
+		},
+	},
+	{
+		type: "music",
+		title: "一生所爱",
+		artist: "卢冠廷/莫文蔚",
+		cover:
+			"https://y.qq.com/music/photo_new/T002R300x300M000001UAAKE4QJguW_3.jpg?max_age=2592000",
+		album: "大话西游",
+		date: "1995-05-01",
+		platform: {
+			qqmusic:
+				"https://i.y.qq.com/v8/playsong.html?songid=1727258#webchat_redirect",
+		},
+	},
+	{
+		type: "photo",
+		title: "京都红叶",
+		image:
+			"https://gcore.jsdelivr.net/gh/Keduoli03/My_img@img/FYwAUt5aQAU07-a.jpg",
+		date: "2023-11-22",
+		location: "清水寺",
+		camera: "Sony A7IV",
+		tags: ["旅行", "秋天"],
+	},
+	{
+		type: "photo",
+		title: "依蕾娜",
+		image: "https://gcore.jsdelivr.net/gh/Keduoli03/My_img@img/img/yileina.png",
+		date: "2023-11-22",
+		location: "日本",
+		camera: "Sony A7IV",
+		tags: ["动漫", "魔女"],
+	},
+];
---
+++ src/data/friendsData.js
@@ -0,0 +1,61 @@
+export const items = [
+	{
+		title: "Astro",
+		imgurl: "https://avatars.githubusercontent.com/u/44914786?s=48&v=4",
+		desc: "一个现代化的静态站点生成框架",
+		siteurl: "https://github.com/withastro/astro",
+		tags: ["框架"],
+	},
+	{
+		title: "Fuwari",
+		imgurl:
+			"https://gcore.jsdelivr.net/gh/Keduoli03/My_img@img/demo-avatar.png",
+		desc: "基于 Astro 开发的静态博客模板",
+		siteurl: "https://github.com/saicaca/fuwari",
+		tags: ["主题"],
+	},
+	{
+		title: "天影大侠的小站",
+		imgurl:
+			"https://weavatar.com/avatar/7388ea4765a319d5f83fea61c3236384?s=1000",
+		desc: "The Blog of SkyShadowHero!",
+		siteurl: "https://blog.skyshadow.fun/",
+		tags: ["友链"],
+	},
+	{
+		title: "伊卡的记事本",
+		imgurl: "https://ikamusume7.org/avatar.webp",
+		desc: "又菜又爱学",
+		siteurl: "https://ikamusume7.org",
+		tags: ["友链"],
+	},
+	{
+		title: "M次元",
+		imgurl: "https://mmxza.com/usr/uploads/2025/03/2201968370.gif",
+		desc: "小声记录小事",
+		siteurl: "https://mmxza.com/",
+		tags: ["友链"],
+	},
+	{
+		title: "JIPA233の小窝",
+		imgurl: "https://jipa.moe/static/avatar.jpg",
+		desc: "Deed divides beings into lower and higher ones.",
+		siteurl: "https://jipa.moe",
+		tags: ["友链"],
+	},
+	{
+		title: "岚天小窝",
+		imgurl:
+			"https://gravatar.ltya.top/avatar/5af06b461740fb2bd7467b8561399703?s=500",
+		desc: "一只鸽子住在这里~",
+		siteurl: "https://blog.ltya.top/",
+		tags: ["友链"],
+	},
+	{
+		title: "LineXic书屋",
+		imgurl: "https://linexic.top/avatar.webp",
+		desc: "难离难舍，想抱紧些",
+		siteurl: "https://www.linexic.top/",
+		tags: ["友链"],
+	},
+];
