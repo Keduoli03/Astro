9726247747512238de3465e5e4509e06a386a9d5
Keduoli03 <2801429414@qq.com>
2025-05-24 22:07:24 +0800

    Update chronoboxData.js

--- src/data/chronoboxData.js
+++ src/data/chronoboxData.js
@@ -1,13 +1,24 @@
 export const timelineItems = [
 	{
 		type: "anime",
+		title: "薇薇 -萤石眼之歌",
+		cover:
+			"https://gcore.jsdelivr.net/gh/Keduoli03/My_img@img/%E8%90%A4%E7%9F%B3%E7%9C%BC%E4%B9%8B%E6%AD%8C.webp",
+		date: "2025-04-13",
+		url: "https://www.blueke.top/posts/%E4%BA%BA%E4%B8%8Eai%E7%88%B1%E4%B8%8E%E7%90%86%E6%80%A7/",
+		episodes: "连载中",
+		rating: 8.6,
+		tags: ["喜剧", "剧情", "成长"],
+	},
+	{
+		type: "anime",
 		title: "时光流逝，饭菜依旧美味",
 		cover:
 			"https://gcore.jsdelivr.net/gh/Keduoli03/My_img@img/%E6%97%B6%E5%85%89%E6%B5%81%E9%80%9D%EF%BC%8C%E9%A5%AD%E8%8F%9C%E4%BE%9D%E6%97%A7%E7%BE%8E%E5%91%B3.webp",
 		date: "2025-04-13",
 		url: "https://baike.baidu.com/item/%E5%B2%81%E6%9C%88%E6%B5%81%E9%80%9D%E9%A5%AD%E8%8F%9C%E4%BE%9D%E6%97%A7%E7%BE%8E%E5%91%B3/65543356",
 		episodes: "连载中",
-		rating: 4.9,
+		rating: 9.0,
 		tags: ["喜剧", "剧情", "成长"],
 	},
 	{
@@ -18,7 +29,7 @@ export const timelineItems = [
 		date: "2016-04-04",
 		url: "https://mzh.moegirl.org.cn/Re:%E4%BB%8E%E9%9B%B6%E5%BC%80%E5%A7%8B%E7%9A%84%E5%BC%82%E4%B8%96%E7%95%8C%E7%94%9F%E6%B4%BB",
 		episodes: "更新至第三季下半",
-		rating: 4.9,
+		rating: 9.9,
 		tags: ["奇幻", "冒险", "剧情", "成长"],
 	},
 	{
@@ -29,7 +40,7 @@ export const timelineItems = [
 		date: "2023-09-15",
 		url: "https://mzh.moegirl.org.cn/%E8%BF%9B%E5%87%BB%E7%9A%84%E5%B7%A8%E4%BA%BA",
 		episodes: 25,
-		rating: 4.8,
+		rating: 8.8,
 		tags: ["热血", "奇幻"],
 	},
 	{
@@ -39,7 +50,7 @@ export const timelineItems = [
 		date: "2008-10-01",
 		url: "https://mzh.moegirl.org.cn/%E9%BE%99%E4%B8%8E%E8%99%8E",
 		episodes: 25,
-		rating: 4.8,
+		rating: 8.5,
 		tags: ["爱情", "校园"],
 	},
 	{
@@ -51,7 +62,7 @@ export const timelineItems = [
 		desc: "震撼结局，自由的定义引发深思",
 		url: "https://example.com/aot",
 		episodes: 25,
-		rating: 4.8,
+		rating: 7.8,
 		tags: ["爱情", "喜剧"],
 	},
 	{
