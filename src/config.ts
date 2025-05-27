321acedd2a180dbeb4fb0a675d4b36b1c6b2688e
Keduoli03 <2801429414@qq.com>
2025-05-27 22:40:13 +0800

    更新logo

--- src/config.ts
+++ src/config.ts
@@ -29,6 +29,9 @@ export const siteConfig: SiteConfig = {
 		depth: 3, // Maximum heading depth to show in the table, from 1 to 3
 	},
 	favicon: [
+		{
+			src:'/favicon/logo.png'
+		},
 		// Leave this array empty to use the default favicon
 		// {
 		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
