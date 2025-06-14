63f4f5cda9c41ef9e0c1c62e093b2cf658876d32
Keduoli03 <2801429414@qq.com>
2025-06-14 23:54:27 +0800

    增加书籍

--- src/data/chronoboxData.js
+++ src/data/chronoboxData.js
@@ -1,4 +1,16 @@
 export const timelineItems = [
+	 {
+    type: "book",
+    title: '牧神记',
+    cover:
+			"https://gcore.jsdelivr.net/gh/Keduoli03/My_img@img/%E7%89%A7%E7%A5%9E%E8%AE%B0.webp",
+    author: '宅猪',
+    date: '2017-6-20',
+    desc: '大墟的祖训说，天黑，别出门。',
+    url: 'https://book.douban.com/subject/2582846/',
+    rating: 9.1,
+    tags: ['玄幻', '东方']
+  },
 	{
 		type: "anime",
 		title: "薇薇 -萤石眼之歌",
